44d26e938e0574dd29c0701f2c3785d3fc3638f0
Eva Medvedeva <ohmywob@gmail.com>
2017-03-10 15:18:22 +0300

    egit fixed and initial bootstrap works

--- Gemfile
+++ Gemfile
@@ -9,13 +9,15 @@ ruby '2.4.0'
 gem 'rails', '~> 5.0.1'
 gem 'sqlite3'
 gem 'puma', '~> 3.0'
-gem 'sass-rails', '~> 5.0'
+gem 'bootstrap-sass', '~> 3.3.6'
+gem 'sass-rails', '>= 3.2'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'jquery-rails'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'devise', '~> 4.2'
+
 
 group :development, :test do
   gem 'byebug', platform: :mri
--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,6 +2,7 @@ class LinksController < ApplicationController
   before_action :set_link, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :authorized_user, only: [:edit, :update, :destroy]
+
   # GET /links
   # GET /links.json
   def index
@@ -62,10 +63,27 @@ class LinksController < ApplicationController
     end
   end
 
+  def upvote
+    @link = Link.find(params[:id])
+    @link.upvote_by current_user
+    redirect_to :back
+  end
+
+  def downvote
+    @link = Link.find(params[:id])
+    @link.downvote_from current_user
+    redirect_to :back
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_link
       @link = Link.find(params[:id])
+    end
+
+    def authorized_user
+      @link = current_user.links.find_by(id: params[:id])
+      redirect_to links_path, notice: "Not authorized to edit this link" if @link.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
