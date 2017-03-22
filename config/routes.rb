4382c895f062868ab201e421ccd169c98176af63
Evanto <Evanto@users.noreply.github.com>
2017-03-22 14:01:51 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :comments
   devise_for :users
+  
+   # next bit of code is for gem act_as_votable, more info in this tutorial: http://www.mattmorgante.com/technology/votable
   resources :links do
      member do
        put "like", to:    "links#upvote"
@@ -11,4 +13,5 @@ Rails.application.routes.draw do
 
   root to: "links#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 
 end
